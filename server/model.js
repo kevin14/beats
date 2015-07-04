731d33dcab79b15e8e50426c627b3efb9b389b6c
Mike Bodge <mbodge@gmail.com>
2015-07-04 00:08:58 -0700

    upload flow

--- server/model.js
+++ server/model.js
@@ -2,10 +2,10 @@ var mongoose = require('mongoose');
 var ObjectId = mongoose.Schema.ObjectId;
 
 var uploadsSchema = mongoose.Schema({
-    'file_id' : String,
-    'city'    : String,
-    'url'     : String
-
+    'file_id'     : String,
+    'city'        : String,
+    'url'         : String,
+    'created_at'  : { type: Date, default: Date.now }
 });
 
 var uploads = mongoose.model('uploads', uploadsSchema);
