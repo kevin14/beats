b8019757794ce517aba8c8eb6b23e7b19a737ed4
mbodge <mbodge@gmail.com>
2015-07-07 15:10:45 -0700

    video intro

--- server/public/js/app.js
+++ server/public/js/app.js
@@ -1,12 +1,27 @@
-var WW, WH;
+var WW, WH; // window height / width
+var BV; // big video initIntro
 
 $(document).ready(function(){
 
   resizeMe();
   $(window).on("resize", resizeMe);
 
+  initIntro();
+
 });
 
+function initIntro() {
+  BV = new $.BigVideo({container: $('#intro-video')});
+  BV.init();
+  BV.show('http://vjs.zencdn.net/v/oceans.mp4',{doLoop:false});
+  BV.getPlayer().on("ended", function () {
+    showEditor();
+  });
+}
+
+function showEditor() {
+  console.log("editor");
+}
 
 function resizeMe() {
     WW = $(window).width();
