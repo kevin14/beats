8560ffa513624b71993417f8e04eca38e42f52ab
Roger Braunstein <roger@partlyhuman.com>
2015-07-22 15:31:09 -0700

    Add sharing choice popup #45

--- assets/js/editor-deps.js
+++ assets/js/editor-deps.js
@@ -1,6 +1,7 @@
 //= require blueimp-canvas-to-blob/js/canvas-to-blob
 //= require file-saver-saveas-js/FileSaver
 //= require nouislider/distribute/nouislider
+//= require featherlight/release/featherlight.min
 
 //= require editor
 //= require uploader
